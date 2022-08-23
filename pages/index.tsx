26b908bb08b032a85f475be1b586ab6447c743eb
Carlos <carloshlzansavio@gmail.com>
2022-08-23 12:22:16 +0100

    extract files page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import useSWR from "swr";
-import { FileDto } from "./api/files";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-  const fetcher = (url: string) => fetch(url).then((res) => res.json());
-  const { data, error } = useSWR<FileDto[]>("/api/files", fetcher);
-
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
-
-  const fileList = data.map(({ id, name, type, url }) => {
-    return (
-      <li key={id}>
-        <div className="columns-3 space-y-4">
-          <div>{name}</div>
-          <div>{type}</div>
-          <div>
-            <a href={url} className="text-blue-500">
-              Url
-            </a>
-          </div>
-        </div>
-      </li>
-    );
-  });
+  const [file, setFile] = useState<File | undefined>(undefined);
 
   return (
     <div>
@@ -35,15 +14,7 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="container mx-auto">
-        <ul>
-          <div className="columns-3 space-y-4">
-            <div className="font-medium">Name</div>
-            <div className="font-medium">Type</div>
-            <div className="font-medium">Url</div>
-          </div>
-
-          {fileList}
-        </ul>
+        <h1>index page</h1>
       </div>
     </div>
   );
